46a3d34497ea2b23ca2e72e96a9bc453de20aa2d
Sahil Malhotra <Sahilmalhotra92@gmail.com>
2021-02-14 21:42:04 +1300

    Added name and age validation

--- src/main/java/org/sahil/embl/person/model/Person.java
+++ src/main/java/org/sahil/embl/person/model/Person.java
@@ -7,23 +7,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Person {
 
     @NotNull(message = "firstName cannot be null")
     @NotBlank(message = "firstName cannot be blank")
+    @Size(max = 256, min = 2)
     @JsonProperty("firstName")
     private String firstName;
 
     @NotNull(message = "lastName cannot be null")
     @NotBlank(message = "lastName cannot be blank")
+    @Size(max = 256, min = 2)
     @JsonProperty("lastName")
     private String lastName;
 
     @NotNull(message = "age cannot be null")
     @NotBlank(message = "age cannot be blank")
-    @Pattern(regexp = "^(0|[1-9][0-9]*)$", message = "must be number")
+    @Pattern(regexp = "^(\\d{0,2}(\\.\\d{1,2}\u200B)?|100(\\.00?)?)$", message = "must be number and less than 100")
     @JsonProperty("age")
     private String age;
 
